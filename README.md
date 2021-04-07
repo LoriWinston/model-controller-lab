64b9ba5862597bfa8386768523a69623828cb8e5
Lori Winston <Lcwinston@gmail.com>
2021-04-07 12:07:00 -0700

    added sql table

---
+++ README.md
@@ -0,0 +1,20 @@
+-Model
+
+-Controller
+
+
+write a test for each crud route
+
+POST
+GET
+GET BY ID
+PUT
+DELETE
+
+Build a SQL Table
+-Birds
+-name
+-age
+-color
+
+
--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,32 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { Router } = require("express");
+const Bird = require('../lib/models/Bird');
 
 describe('model-controller-lab routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  let bird;
+  beforeEach(async () => {
+    bird = await Bird.insert({  name: 'eagle', age: 3, color: 'golden' });
+
+  });
+
+  it.only ('adds a bird to the database', async () => {
+    const res = await request(app)
+      .post('/api/v1/birds')
+      .send({ name: 'falcon', age: 5, color: 'brown' });
+
+    expect(res.body).toEqual({ 
+      id: expect.any(String),
+      name: 'falcon', 
+      age: 5, 
+      color: 'brown', 
+    });
+  });
+
+
 });
