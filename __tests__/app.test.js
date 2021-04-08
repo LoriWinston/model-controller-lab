f72d6a23c498df1dcb348690bfc49c2807e3d425
Lori Winston <Lcwinston@gmail.com>
2021-04-08 09:36:56 -0700

    first two tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,12 +11,12 @@ describe('model-controller-lab routes', () => {
   });
 
   let bird;
-  beforeEach(async () => {
+  beforeEach ( async () => {
     bird = await Bird.insert({  name: 'eagle', age: 3, color: 'golden' });
 
   });
 
-  it.only ('adds a bird to the database', async () => {
+  it('adds a bird to the database', async () => {
     const res = await request(app)
       .post('/api/v1/birds')
       .send({ name: 'falcon', age: 5, color: 'brown' });
@@ -27,6 +27,19 @@ describe('model-controller-lab routes', () => {
       age: 5, 
       color: 'brown', 
     });
+  });
+
+  it('gets a list of all birds', async () => {
+    const res = await request(app)
+    .get('/api/v1/birds');
+
+    expect(res.body).toEqual([{
+          "id": "1", 
+      "name": "eagle",
+      "age": 3,
+      "color": "golden"
+
+    }]);
   });
 
 
